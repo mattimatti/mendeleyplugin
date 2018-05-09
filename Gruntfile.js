6f29ae98f116307f4f1c6f83e0e57695fdcbdf79
mattimatti <mmonti@gmail.com>
2018-05-09 12:29:30 +0200

    edited grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,146 +5,156 @@
 
 module.exports = function(grunt) {
 
-    require('load-grunt-tasks')(grunt);
+	require('load-grunt-tasks')(grunt);
 
-    grunt
-	    .initConfig({
+	grunt
+		.initConfig({
 
-		pkg : grunt.file.readJSON('package.json'),
+			pkg: grunt.file.readJSON('package.json'),
 
-		'phpunit-runner' : {
-		    all : {
-			options : {
-			    phpunit : 'vendor/bin/phpunit',
-			    bootstrap : 'vendor/autoload.php',
-			    testdoxText : true
+			'phpunit-runner': {
+				all: {
+					options: {
+						phpunit: 'vendor/bin/phpunit',
+						bootstrap: 'vendor/autoload.php',
+						testdoxText: true
+					},
+					testFolder: 'tests/'
+				}
 			},
-			testFolder : 'tests/'
-		    }
-		},
 
-		webpack : {
-		    app : {
-			entry : "./public/assets/js/src/app.module.js",
-			output : {
-				path : "./public/assets/js/dist",
-				filename : "app.js",
-			}
-		    }
-		},
-
-		uglify : {
-		    options : {
-			compress : {
-//			    drop_console : true
-			}
-		    },
-		    app : {
-			files : {
-			    './public/assets/js/dist/app.min.js' : [ './public/assets/js/dist/app.js' ]
-			}
-		    }
-		},
-
-		less : {
-		    styles : {
-			options : {
-			    paths : [ "css/src" ]
+			webpack: {
+				app: {
+					entry: "./public/assets/js/src/app.module.js",
+					output: {
+						path: "./public/assets/js/dist",
+						filename: "app.js",
+					}
+				}
 			},
-			files : {
-			    "./public/assets/css/dist/app.css" : "./public/assets/css/src/app.less"
-			}
-		    }
-		},
-		
-		clean: {
-		    contents: ['./wordpress/wp-content/plugins/mmmendeleyplugin/*'],
-		},
 
-		copy : {
-		    main : {
-			expand : true,
-			cwd : '.',
-			src : [ '*.php', 'img/**/*', 'public/**/*', 'includes/**/*', 'languages/**/*', 'admin/**/*', 'assets/**/*', 'doc/**/*' ],
-			dest : './wordpress/wp-content/plugins/mmmendeleyplugin/',
-		    },
-		},
-
-		compress : {
-		    main : {
-			options : {
-			    archive : './wordpress/wp-content/plugins/mmmendeleyplugin.zip'
+			uglify: {
+				options: {
+					compress: {
+						//			    drop_console : true
+					}
+				},
+				app: {
+					files: {
+						'./public/assets/js/dist/app.min.js': ['./public/assets/js/dist/app.js']
+					}
+				}
 			},
-			files : [ {
-			    expand : true,
-			    cwd : '.',
-			    src : [ '*.php', 'img/**/*', 'public/**/*', 'includes/**/*', 'languages/**/*', 'admin/**/*', 'assets/**/*', 'doc/**/*' ],
-			    dest : '.'
-			} ]
-		    }
-		},
 
-		watch : {
+			less: {
+				styles: {
+					options: {
+						paths: ["css/src"]
+					},
+					files: {
+						"./public/assets/css/dist/app.css": "./public/assets/css/src/app.less"
+					}
+				}
+			},
 
-		    gui : {
-			files : [ 'public/assets/css/src/**/*.less', 'includes/*.html', 'public/assets/js/dist/*.js', '**/*.php' ],
-			tasks : [ 'deploy' ],
-			options : {
-			    spawn : false,
+			clean: {
+				contents: ['./wordpress/wp-content/plugins/mmmendeleyplugin/*'],
+			},
+
+			copy: {
+				main: {
+					expand: true,
+					cwd: '.',
+					src: ['*.php', 'img/**/*', 'public/**/*', 'includes/**/*', 'languages/**/*', 'admin/**/*', 'assets/**/*', 'doc/**/*'],
+					dest: './wordpress/wp-content/plugins/mmmendeleyplugin/',
+				},
+			},
+
+			compress: {
+				main: {
+					options: {
+						archive: './wordpress/wp-content/plugins/mmmendeleyplugin.zip'
+					},
+					files: [{
+						expand: true,
+						cwd: '.',
+						src: ['*.php', 'img/**/*', 'public/**/*', 'includes/**/*', 'languages/**/*', 'admin/**/*', 'assets/**/*', 'doc/**/*'],
+						dest: '.'
+					}]
+				}
+			},
+
+			watch: {
+
+				gui: {
+					files: ['public/assets/css/src/**/*.less', 'includes/*.html', 'public/assets/js/dist/*.js', '**/*.php'],
+					tasks: ['deploy'],
+					options: {
+						spawn: false,
+					}
+				}
+			},
+
+			bump: {
+				options: {
+					files: ['package.json'],
+					updateConfigs: ['pkg', 'component'],
+					commit: true,
+					commitMessage: 'Release v%VERSION%',
+					commitFiles: ['-a'],
+					createTag: true,
+					tagName: 'v%VERSION%',
+					tagMessage: 'Version %VERSION%',
+					push: true,
+					pushTo: 'origin',
+					globalReplace: true
+				}
+			},
+
+			availabletasks: {
+				tasks: {
+					options: {
+						filter: 'exclude',
+						tasks: ['availabletasks', 'tasks']
+					}
+				}
+			},
+
+
+			//		ftpush : {
+			//		    build : {
+			//			auth : {
+			//			    host : 'ftp.fornitoreoffresi.com',
+			//			    port : 21,
+			//			    authKey : 'key1'
+			//			},
+			//			src : '.',
+			//			dest : '/www.fornitoreoffresi.com/wp/wp-content/plugins/standselector',
+			//			exclusions : [ 'node_modules', '.phpintel', '.grunt', '.DS_Store', '.ftppass', '.git*',
+			//				'ftpcache.json' ],
+			//			simple : true,
+			//			cachePath : './ftpcache.json'
+			//		    }
+			//		},
+
+			"regex-replace": {
+				verz: {
+					src: ['./waau-mendeley-plugin.php'],
+					actions: [{
+						name: 'version',
+						search: ' * Version.*$',
+						replace: ' Version: <%= pkg.version %>',
+						flags: 'mg'
+					}]
+				}
 			}
-		    }
-		},
 
-		bump : {
-		    options : {
-			files : [ 'package.json' ],
-			updateConfigs : [ 'pkg', 'component' ],
-			commit : true,
-			commitMessage : 'Release v%VERSION%',
-			commitFiles : [ '-a' ],
-			createTag : true,
-			tagName : 'v%VERSION%',
-			tagMessage : 'Version %VERSION%',
-			push : true,
-			pushTo : 'origin',
-			globalReplace : true
-		    }
-		},
+		});
 
-//		ftpush : {
-//		    build : {
-//			auth : {
-//			    host : 'ftp.fornitoreoffresi.com',
-//			    port : 21,
-//			    authKey : 'key1'
-//			},
-//			src : '.',
-//			dest : '/www.fornitoreoffresi.com/wp/wp-content/plugins/standselector',
-//			exclusions : [ 'node_modules', '.phpintel', '.grunt', '.DS_Store', '.ftppass', '.git*',
-//				'ftpcache.json' ],
-//			simple : true,
-//			cachePath : './ftpcache.json'
-//		    }
-//		},
-
-		"regex-replace" : {
-		    verz : {
-			src : [ './waau-mendeley-plugin.php' ],
-			actions : [ {
-			    name : 'version',
-			    search : ' * Version.*$',
-			    replace : ' Version: <%= pkg.version %>',
-			    flags : 'mg'
-			} ]
-		    }
-		}
-
-	    });
-
-    grunt.registerTask("js", [ "webpack", "uglify" ]);
-    grunt.registerTask("css", [ 'less' ]);
-    grunt.registerTask("build", [ "js", "css" ]);
-    grunt.registerTask("test", [ "build", "clean", "copy" ]);
-    grunt.registerTask('deploy', [ 'build','bump-only',  'regex-replace', 'bump-commit' ]);
+	grunt.registerTask("js", ["webpack", "uglify"]);
+	grunt.registerTask("css", ['less']);
+	grunt.registerTask("build", ["js", "css"]);
+	grunt.registerTask("test", ["build", "clean", "copy"]);
+	grunt.registerTask('deploy', ['build', 'bump-only', 'regex-replace', 'bump-commit']);
 
 };
